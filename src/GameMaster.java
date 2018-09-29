7012d65feb8d548c383eb75bd34df28534a6e9eb
De Vo <dxvo@uh.edu>
2018-09-28 23:18:14 -0500

    update on playerSetup()

--- src/GameMaster.java
+++ src/GameMaster.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 
 public class GameMaster
 {
 	private Map gameMap;
-	private Player playerList[];
 	private BattleHandler battleHandler;
+
+	private ArrayList <Player> playerList;
+
 	private int maxPlayers; // dont think we need this
 	private int numPlayers;
 	private int playerTurn;
@@ -40,19 +42,34 @@ public class GameMaster
 	{
 		// Prompt Number of Players and
 		// Initialize Players
-		int num_player = 0;
 		do {
 
 			Scanner reader = new Scanner(System.in);  // Reading from System.in
 			System.out.print("Enter number of player between 1 and 6: ");
-			num_player = reader.nextInt(); // Scans the next token of the input as an int //once finishe
-			if(num_player <= 0)
+			numPlayers = reader.nextInt(); //numPlayers is an attribution of the GameMaster class
+			if(numPlayers <= 0)
+
 				System.out.println("Number of player cant be smaller than 0");
-			if (num_player > 6)
+			if (numPlayers > 6)
+
 				System.out.println("Number of player cant be greater than 6");
 
-		} while (num_player <= 0 || num_player > 6);
+		} while (numPlayers <= 0 || numPlayers > 6);
 
+		//This is to initialize the playerlist. The PlayerId and numofUnits also initialize
+		playerList = new ArrayList <Player>(); //initialize the playerlist - which is an attribute of this class
+		for (int i = 0 ; i < numPlayers; i++)
+		{
+			Player player = new Player(i); //initialize player ID - start with 0
+			int numUnits = 50 - numPlayers * 5;
+			player.setNumUnits(numUnits);
+			playerList.add(player); // append player into list
+		}
+
+		for (int i=0; i<playerList.size(); i++) {
+			System.out.println(playerList.get(i).getPlayerID());
+			System.out.println(playerList.get(i).getNumUnits()); //playerList.get(i) return object type Player
+		}
 
 
 	}
