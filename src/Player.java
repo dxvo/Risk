054f417f09dd0dec2198be718c94373529400970
De Vo <42792976+dxvo@users.noreply.github.com>
2018-10-02 22:39:30 -0500

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,3 @@
-
 public class Player
 {
 	private int playerID;
@@ -6,14 +5,16 @@ public class Player
 	private int numBenchedUnits;
 	private int numTerritories;
 	
-	public Player() {
+	public Player()
+	{
 		playerID = 0;
 		numUnits = 0;
 		numBenchedUnits = 0;
 		numTerritories = 0;
 	}
 	
-	public Player(int id) {
+	public Player(int id)
+	{
 		playerID = id;
 		numUnits = 0;
 		numBenchedUnits = 0;
@@ -21,42 +22,50 @@ public class Player
 	}
 	
 	// Mutators
-	public void setPlayerID(int id) {
+	public void setPlayerID(int id)
+	{
 		if(id > 0)
 			playerID = id;
 	}
 	
-	public void setNumUnits(int units) {
+	public void setNumUnits(int units)
+	{
 		if(units >= 0)
 			numUnits = units;
 	}
 	
-	public void setNumBenchedUnits(int units) {
+	public void setNumBenchedUnits(int units)
+	{
 		if(units >= 0)
 			numBenchedUnits = units;
 	}
 	
-	public void setNumTerritories(int count) {
+	public void setNumTerritories(int count)
+	{
 		if(count >= 0)
 			numTerritories = count;
 	}
 	
-	public void addBenchedUnits(int count) {
-		if(count < 1)
-			return;
-		numBenchedUnits += count;
-	}
-	
-	public void deployBenchedUnits(int count) {
+	public void addBenchedUnits(int count)
+	{
 		if(count < 1)
 			return;
 		
-		if(numBenchedUnits < count) {
+		numBenchedUnits += count;
+	}
+	
+	public void deployBenchedUnits(int count)
+	{
+		if(count < 1)
+			return;
+		
+		if(numBenchedUnits < count)
+		{
 			numUnits += count - numBenchedUnits;
 			numBenchedUnits = 0;
 		}
-
-		else {
+		else
+		{
 			numUnits += count;
 			numBenchedUnits -= count;
 		}
