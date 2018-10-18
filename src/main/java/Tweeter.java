13898a9fe0186d6ef145d6859a213146351677e5
caaraiza <32781936+caaraiza@users.noreply.github.com>
2018-10-18 10:24:14 -0500

    reworked twitter API. fixed some bugs

--- src/main/java/Tweeter.java
+++ src/main/java/Tweeter.java
@@ -1,13 +1,8 @@
-import twitter4j.GeoLocation;
-import twitter4j.Paging;
-import twitter4j.Query;
-import twitter4j.QueryResult;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 import twitter4j.TwitterException;
 
-import java.io.IOException;
 import java.io.PrintStream;
 
 import java.util.ArrayList;
