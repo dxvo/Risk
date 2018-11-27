3574fc94f61cb8b4d1021c247fdd4f24a758293e
dxvo <dxvo@uh.edu>
2018-11-27 15:10:38 -0600

    add test for Die

---
+++ src/test/java/timer_inputTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class timer_inputTest {
+
+    Player testPlayer = new Player(5);
+
+    @Test
+    public void get_input() {
+
+    }
+}
