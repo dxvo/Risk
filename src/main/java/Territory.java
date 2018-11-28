cdff25b7fe91c441e33a0b19f341e16575a2bc39
dxvo <dxvo@uh.edu>
2018-11-28 02:14:22 -0600

    Add JavaDoc style

--- src/main/java/Territory.java
+++ src/main/java/Territory.java
@@ -1,23 +1,52 @@
+/***
+ * This Territory class is use to store player info
+ * ownerID - the player who owns this territory
+ * numUnits - How many units does this territory currently have
+ *
+ * @author De, Carlos and Will
+ * @version 1.0
+ * @since 2019-10
+ */
 public class Territory
 {
 	private int ownerID;
 	private int numUnits;
-	
+
+	/***
+	 * default constructor
+	 * ownerID is set to -1 because the index is start at 0 to avoid confusion
+	 */
 	public Territory()
 	{
 		ownerID = -1;
 		numUnits = 0;
 	}
-	
+
+	/***
+	 * setter method
+	 * @param id - assign the owner ID for this territory
+	 */
 	public void setOwnerID(int id) { ownerID = id; }
 
+	/***
+	 * getter method
+	 * @return return the ID of play who currenly owns this territory
+	 */
 	public int getOwnerID()
 	{
 		return ownerID;
 	}
 
+	/***
+	 * setter method
+	 * @param units - The unumber of units that needs to be set for this territory
+	 */
 	public void setNumUnits(int units){ numUnits = units;}
 
+	/***
+	 * getter method
+	 * @return - The Number of Units that this territory currenly stores
+	 */
 	public int getNumUnits() { return numUnits;}
 
 }
--- src/main/java/Tweeter.java
+++ src/main/java/Tweeter.java
@@ -3,31 +3,43 @@ import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 import twitter4j.TwitterException;
 import java.io.PrintStream;
-
 import java.util.ArrayList;
 import java.util.List;
 
+/***
+ * This Class is used to tweet out the game result such as end of each battle, each turn or end of game
+ * The class using Tweeter API with private accessKey
+ *
+ * @author Carlos
+ * @version 1.2
+ * @since 2018-10
+ */
 public class Tweeter
 {
     private Twitter twitter;
     private PrintStream consolePrint;
     private List<Status> statuses;
 
-
-
+    /***
+     * Constructor method
+     * Makes an instance of twitter - this is re-usable and thread safe
+     * This connects to twitter and performs authorization
+     * @param console -
+     */
     public Tweeter(PrintStream console)
     {
-        //Makes an instance of twitter - this is re-usable and thread safe
-        //connects to twitter and performs authorization
-        //twitter = new TwitterFactory().getInstance();
         twitter = TwitterFactory.getSingleton();
         consolePrint = console;
         statuses = new ArrayList<Status>();
     }
 
-    /* *********** Part 1 ********* */
-    /* This method will tweet a given message */
-    public void tweetOut(String message) throws TwitterException//, IOException
+
+    /***
+     * This method will tweet a given message
+     * @param message the message that will be send after each event
+     * @throws TwitterException
+     */
+    public void tweetOut(String message) throws TwitterException
     {
             Status status = twitter.updateStatus(message);
             System.out.println("Successfully updated the status to [" + status.getText() + ']');
